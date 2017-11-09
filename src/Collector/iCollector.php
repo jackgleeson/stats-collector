52825eeb6a8139d9f96ccd4adfb594a65950cb18
Jack Gleeson <me@jackgleeson.co.uk>
2017-11-09 18:11:12 +0000

    updated iCollector interface to remove optional arguments from method signatures

--- src/Collector/iCollector.php
+++ src/Collector/iCollector.php
@@ -7,13 +7,13 @@ interface iCollector
 
     public static function getInstance();
 
-    public function addStat($name, $value, $options = null);
+    public function addStat($name, $value, $options);
 
     public function removeStat($namespace);
 
-    public function getStat($namespace, $withKeys = null);
+    public function getStat($namespace, $withKeys);
 
-    public function getStats(array $namespaces, $withKeys = null);
+    public function getStats(array $namespaces, $withKeys);
 
     public function getStatCount($namespace);
 
